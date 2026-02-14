a803a618e6be9fc65f621f67c6efb22a230f5171
Guilherme Machado Neves <gui.m.neves01@gmail.com>
2026-02-14 01:37:47 -0100

    Update data-source.ts

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -4,8 +4,12 @@ import { Client } from "./entities/Client";
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    url: process.env.DATABASE_URL, 
-    synchronize: true, 
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT) || 6543,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    synchronize: true,
     logging: true,
     entities: [Client],
     ssl: {
